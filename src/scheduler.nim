014b15b61744dc634ea1b3e209e46fddb7ebe543
soasme <soasme@gmail.com>
2019-09-25 09:29:38 +1200

    add docs.

--- src/scheduler.nim
+++ src/scheduler.nim
@@ -1,8 +1,17 @@
 type
-  Beater = ref object of RootObj ## Beater generates beats for the next runs.
+  Beater* = ref object of RootObj ## Beater generates beats for the next runs.
 
-  Runner = ref object of RootObj ## Runner runs the tasks.
+  Runner* = ref object of RootObj ## Runner runs the tasks.
 
-  Storage = ref object of RootObj ## Storage stores tasks definitions.
+  Storage* = ref object of RootObj ## Storage stores tasks definitions.
 
-  Scheduler = ref object of RootObj ## Scheduler schedules the tasks.
+  Scheduler* = ref object of RootObj ## Scheduler acts as an event loop and schedules all the tasks.
+
+proc next*(beater: Beater): float = 0.0
+
+
+type
+  Cron* = ref object of Beater
+
+type
+  Interval* = ref object of Beater
