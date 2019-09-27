a2de5f61226fa0bfc11ba3aeb4fc515dad4f715c
soasme <soasme@gmail.com>
2019-09-27 22:33:31 +1200

    add async task.

--- src/scheduler.nim
+++ src/scheduler.nim
@@ -1,18 +1,17 @@
+## 
+## Basic Concepts
+##
+##
 import threadpool
 import asyncdispatch
-import sugar
+import asyncfutures
 import times
-
-type
-  Task* = ref object
-    id*: string
-    f*: proc (): Future[void]
 
 type
   Beater* = ref object of RootObj ## Beater generates beats for the next runs.
 
 method nextTime*(self: Beater, asOf: DateTime, prev: DateTime): DateTime {.base.} =
-  echo("NotImplementedError: newTick"); quit(1)
+  0.fromUnix.utc
 
 type
   CronBeater* = ref object of Beater
@@ -20,11 +19,93 @@ type
 type
   IntervalBeater* = ref object of Beater
     interval*: TimeInterval
-    start_time*: ref DateTime
-    end_time*: ref DateTime
 
 method nextTime*(self: IntervalBeater, asOf: DateTime, prev: DateTime): DateTime =
   prev + self.interval
+
+type
+  ExecutorKind* = enum
+    ekThread,
+    ekProcess,
+    ekAsync
+
+  TaskBase* = ref object of RootObj
+    id: string
+    beater: Beater
+
+  Task*[TArg] = ref object of TaskBase
+
+  ThreadedTask*[TArg] = ref object of Task[TArg]
+    thread: Thread[TArg]
+    when TArg is void:
+      fn: proc () {.nimcall, gcsafe.}
+    else:
+      fn: proc (arg: TArg) {.nimcall, gcsafe.}
+      arg: TArg
+
+  AsyncTask*[TArg] = ref object of Task[TArg]
+    future: Future[void]
+    when TArg is void:
+      fn: proc (): Future[void] {.nimcall.}
+    else:
+      fn: proc (arg: TArg): Future[void] {.nimcall.}
+      arg: TArg
+
+proc newThreadedTask*(fn: proc() {.thread, nimcall.}, beater: Beater, id=""): ThreadedTask[void] =
+  var thread: Thread[void]
+  result = ThreadedTask[void](id: id, thread: thread, fn: fn, beater: beater)
+
+proc newThreadedTask*[TArg](
+  fn: proc(arg: TArg) {.thread, nimcall.},
+  arg: TArg,
+  beater: Beater,
+  id=""
+): ThreadedTask[TArg] =
+  var thread: Thread[TArg]
+  result = ThreadedTask[TArg](id: id, thread: thread, fn: fn, arg: arg, beater: beater)
+
+proc newAsyncTask*(
+  fn: proc(): Future[void] {.nimcall.},
+  beater: Beater,
+  id=""
+): AsyncTask[void] =
+  var future = newFuture[void](id)
+  result = AsyncTask[void](id: id, future: future, fn: fn, beater: beater)
+
+proc newAsyncTask*[TArg](
+  fn: proc(arg: TArg): Future[void] {.nimcall.},
+  arg: TArg,
+  beater: Beater,
+  id=""
+): AsyncTask[TArg] =
+  var future = newFuture[void](id)
+  result = AsyncTask[TArg](id: id, future: future, fn: fn, arg: arg, beater: beater)
+
+proc fire*(task: ThreadedTask[void]) =
+  createThread(task.thread, task.fn)
+
+proc fire*[TArg](task: ThreadedTask[TArg]) =
+  createThread(task.thread, task.fn, task.arg)
+
+proc fire*(task: AsyncTask[void]) {.async.} =
+  var fut = task.fn()
+  task.future = fut
+  yield fut
+  if fut.failed:
+    echo("AsyncTask " & task.id & " fire failed.")
+
+proc fire*[TArg](task: AsyncTask[TArg]) {.async.} =
+  var fut = task.fn(task.arg)
+  task.future = fut
+  yield fut
+  if fut.failed:
+    echo("AsyncTask " & task.id & " fire failed.")
+
+proc running*[TArg](task: ThreadedTask[TArg]) =
+  task.thread.running
+
+proc running*[TArg](task: AsyncTask[TArg]) =
+  not (task.future.finished or task.future.failed)
 
 type
   Runner* = ref object of RootObj ## Runner runs the tasks.
@@ -47,19 +128,30 @@ type
 type
   AsyncScheduler* = ref object of Scheduler
 
-type
-  ScheduleError* = object of Exception
+proc tick(since: DateTime) {.thread.} =
+  echo(now() - since)
 
-proc tick() {.async.} =
-  echo("tick ", now())
+proc tick2() {.thread.} =
+  echo(now())
+
+proc atick() {.async.} =
+  await sleepAsync(1000)
+  echo("async tick")
 
 proc start(self: AsyncScheduler) {.async.} =
   let beater = IntervalBeater(interval: TimeInterval(seconds: 1))
-  let task = Task(id: "tick", f: tick)
+  let task = newThreadedTask[DateTime](tick, now(), beater=beater)
+  let atask = newAsyncTask(atick, beater=beater)
+  #let task = newThreadedTask(tick2, beater)
+  var prev = now()
   while true:
-    let fut = task.f()
-    #asyncCheck(fut)
+    asyncCheck atask.fire()
+    task.fire()
+
+    echo(task.thread.running)
     await sleepAsync(1000)
+    echo(task.thread.running)
+    prev = now()
 
 let sched = AsyncScheduler()
 asyncCheck sched.start()
