29f7acb3bbc7337ee68d8f2acc8eef4dedce4b5d
soasme <soasme@gmail.com>
2021-07-15 21:02:20 +1200

    eval seq expr.

--- src/schedulespkg/cron.nim
+++ src/schedulespkg/cron.nim
@@ -10,20 +10,12 @@ import ./expr
 import ./parser
 import ./field
 
-#proc getNextValue*(field: Field, dt: DateTime): Option[int] =
-  #let minVal = field.minValue(dt)
-  #let maxVal = field.maxValue(dt)
-  #let start = max(field.getValue(dt), minValue)
-  #let next = start
-  #if next <= maxVal:
-    #result = some(next)
-  #else:
-    #result = none(int)
+proc getNext*(expr: Expr, field: Field, dt: DateTime): Option[int];
 
-proc getNextForLastDayOfMonth*(field: Field, dt: DateTime): Option[int] =
+proc getNextForLastDayOfMonth*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   some(getDaysInMonth(dt.month, dt.year))
 
-proc getNextForLastDayOfWeek*(field: Field, dt: DateTime): Option[int] =
+proc getNextForLastDayOfWeek*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   # TODO:
   # algorithm:
   #   get day of week for the last day in month
@@ -31,12 +23,12 @@ proc getNextForLastDayOfWeek*(field: Field, dt: DateTime): Option[int] =
   none(int)
 
 # TODO: rename index to nthdayofweek
-proc getNextForIndex*(field: Field, dt: DateTime): Option[int] =
-  var firstDayForIndex = field.expr.indexer - ord(getDayOfWeek(1, dt.month, dt.year))
+proc getNextForIndex*(expr: Expr, field: Field, dt: DateTime): Option[int] =
+  var firstDayForIndex = expr.indexer - ord(getDayOfWeek(1, dt.month, dt.year))
   if firstDayForIndex <= 0:
     firstDayForIndex += 7
 
-  let nextIndex = firstDayForIndex + (field.expr.index-1) * 7
+  let nextIndex = firstDayForIndex + (expr.index-1) * 7
   let daysInMonth = getDaysInMonth(dt.month, dt.year)
 
   if nextIndex <= daysInMonth and nextIndex >= dt.monthday:
@@ -44,10 +36,10 @@ proc getNextForIndex*(field: Field, dt: DateTime): Option[int] =
   else:
     none(int)
 
-proc getNextForRange*(field: Field, dt: DateTime): Option[int] =
+proc getNextForRange*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   let fieldVal = field.getValue(dt)
-  let fieldMin = max(field.minValue(dt), field.expr.rangeSlice.a)
-  let fieldMax = min(field.maxValue(dt), field.expr.rangeSlice.b)
+  let fieldMin = max(field.minValue(dt), expr.rangeSlice.a)
+  let fieldMax = min(field.maxValue(dt), expr.rangeSlice.b)
 
   let nextVal = max(fieldMin, fieldVal)
 
@@ -56,13 +48,13 @@ proc getNextForRange*(field: Field, dt: DateTime): Option[int] =
   else:
     none(int)
 
-proc getNextForStepRange*(field: Field, dt: DateTime): Option[int] =
+proc getNextForStepRange*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   let fieldVal = field.getValue(dt)
-  let fieldMin = max(field.minValue(dt), field.expr.stepExpr.rangeSlice.a)
-  let fieldMax = min(field.maxValue(dt), field.expr.stepExpr.rangeSlice.b)
+  let fieldMin = max(field.minValue(dt), expr.stepExpr.rangeSlice.a)
+  let fieldMax = min(field.maxValue(dt), expr.stepExpr.rangeSlice.b)
 
   var nextVal = max(fieldMin, fieldVal)
-  let offset = field.expr.step - (nextVal - fieldMin) mod field.expr.step
+  let offset = expr.step - (nextVal - fieldMin) mod expr.step
   nextVal += offset
 
   if nextVal <= fieldMax:
@@ -70,7 +62,7 @@ proc getNextForStepRange*(field: Field, dt: DateTime): Option[int] =
   else:
     none(int)
 
-proc getNextForAll*(field: Field, dt: DateTime): Option[int] =
+proc getNextForAll*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   let fieldMin = field.minValue(dt)
   let fieldMax = field.maxValue(dt)
   let nextVal = max(field.getValue(dt), fieldMin)
@@ -79,34 +71,86 @@ proc getNextForAll*(field: Field, dt: DateTime): Option[int] =
   else:
     none(int)
 
-proc getNextForStepAll*(field: Field, dt: DateTime): Option[int] =
+proc getNextForStepAll*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   let fieldMin = field.minValue(dt)
   let fieldMax = field.maxValue(dt)
   var nextVal = max(field.getValue(dt), fieldMin)
-  let offset = field.expr.step - (nextVal - fieldMin) mod field.expr.step
+  let offset = expr.step - (nextVal - fieldMin) mod expr.step
   nextVal += offset
   if nextVal <= fieldMax:
     some(nextVal)
   else:
     none(int)
 
+proc getNextForNum*(expr: Expr, field: Field, dt: DateTime): Option[int] =
+  let nextVal = field.getValue(dt)
+  if nextVal <= expr.num:
+    some(nextVal)
+  else:
+    none(int)
+
+proc getNextForSeq*(expr: Expr, field: Field, dt: DateTime): Option[int] =
+  result = none(int)
+  for subExpr in expr.exprs:
+    let next = getNext(subExpr, field, dt)
+    if next.isNone:
+      continue
+    if result.isNone:
+      result = next
+      continue
+    result = some(min(result.get, next.get))
+
 # TODO: getNextForAny
 # TODO: getNextForSeq
 # TODO: getNextForHash
-# TODO: getNextForNum
 # TODO: getNextForNearest
+
+proc getNext*(expr: Expr, field: Field, dt: DateTime): Option[int] =
+  case expr.kind
+  of ekNum: getNextForNum(expr, field, dt)
+  of ekIndex: getNextForIndex(expr, field, dt)
+  of ekAll: getNextForAll(expr, field, dt)
+  of ekRange: getNextForRange(expr, field, dt)
+  of ekSeq: getNextForSeq(expr, field, dt)
+  of ekStep:
+    case expr.stepExpr.kind
+    of ekAll: getNextForStepAll(expr, field, dt)
+    of ekRange: getNextForStepRange(expr, field, dt)
+    else: none(int)
+  of ekLast:
+    case field.kind
+    of fkDayOfMonth: getNextForLastDayOfMonth(expr, field, dt)
+    of fkDayOfWeek: getNextForLastDayOfWeek(expr, field, dt)
+    else: none(int)
+  else: none(int)
 
 when isMainModule:
   let dt = now()
-  var f = Field(kind: fkDayOfMonth, expr: newLastExpr())
-  echo f.getNextForLastDayOfMonth(dt).get
-  f = Field(kind: fkDayOfWeek, expr: parseDayOfWeeks("4#3"))
-  echo f.getNextForIndex(dt).get
-  f = Field(kind: fkHour, expr: parseHours("23-23"))
-  echo f.getNextForRange(dt).get
-  f = Field(kind: fkMonth, expr: parseMonths("1-12/2"))
-  echo f.getNextForStepRange(dt).get
-  f = Field(kind: fkMonth, expr: parseMonths("*"))
-  echo f.getNextForAll(dt).get
-  f = Field(kind: fkMonth, expr: parseMonths("*/2"))
-  echo f.getNextForStepAll(dt).get
+
+  var e = newLastExpr()
+  var f = Field(kind: fkDayOfMonth, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseDayOfWeeks("4#3")
+  f = Field(kind: fkDayOfWeek, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseHours("0-23")
+  f = Field(kind: fkHour, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseMonths("1-12/2")
+  f = Field(kind: fkMonth, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseMonths("*")
+  f = Field(kind: fkMonth, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseMonths("*/2")
+  f = Field(kind: fkMonth, expr: e)
+  echo getNext(e, f, dt).get
+
+  e = parseHours("1-3,22-23,1-12/2")
+  f = Field(kind: fkHour, expr: e)
+  echo getNext(e, f, dt).get
