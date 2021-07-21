711034d27226e15d83ac76e82641fbd7748202bd
soasme <soasme@gmail.com>
2021-07-21 22:50:38 +1200

    bugfix: handle cross-month day addition.

--- src/schedulespkg/cron/cron.nim
+++ src/schedulespkg/cron/cron.nim
@@ -100,13 +100,14 @@ proc getNextForStepAll*(expr: Expr, field: Field, dt: DateTime): Option[int] =
   let fieldMax = field.maxValue(dt)
   var nextVal = max(fieldVal, fieldMin)
   let offset = (expr.step - (fieldVal - fieldMin)) mod expr.step
-  echo(("get next for step all", nextVal, fieldMin, fieldMax, offset))
   nextVal += offset
   if offset < 0:
     nextVal += expr.step
 
   if nextVal <= fieldMax:
     some(nextVal)
+  elif field.kind == fkDayOfMonth: # cross-month - add enough days
+    some(nextVal - fieldMax)
   else:
     some(fieldMin)
 
@@ -252,7 +253,6 @@ proc getNext*(cron: Cron, dt: DateTime): Option[DateTime] =
     dayOfWeekOffset
   else:
     min(dayOfMonthOffset, dayOfWeekOffset)
-  echo(("day offset", startTime, dayOfMonthOffset))
   startTime += daysOffset.days
 
   let monthsOffset = cron.getInterval(fkMonth, startTime)
